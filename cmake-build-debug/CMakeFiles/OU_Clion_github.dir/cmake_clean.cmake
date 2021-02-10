6014c2d629b9d20a030908977f00c76adfb2d6cb
Michal <michaljedryka00@gmail.com>
2021-02-10 19:26:49 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_Clion_github.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_Clion_github.dir/main.cpp.obj"
+  "OU_Clion_github.exe"
+  "OU_Clion_github.exe.manifest"
+  "OU_Clion_github.pdb"
+  "libOU_Clion_github.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_Clion_github.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OU_Clion_github.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OU_Clion_github.
+# This may be replaced when dependencies are built.
